e0286c3bbe6bf8749a33b0dc1d2d14dfe31e520a
kntowd <kenta2231989@gmail.com>
2022-03-13 12:40:42 +0900

    fizz buzz コードを変更

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const fizzBuzz = () => {
-  for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
+const fizzBuzz = (num) => {
+    if (num % 3 == 0 && num % 5 == 0) {
       return 'FizzBuzz'
     }
 
-    if (i % 3 == 0) {
+    if (num % 3 == 0) {
       return 'Fizz'
     }
 
-    if (i % 5 == 0) {
+    if (num % 5 == 0) {
       return 'Buzz'
     }
 
-    return i
-  }
+    return num
 };
 
 module.exports = fizzBuzz;
